98fe185a44e734933b60c98570ed6ce47c43a0db
IamQinnn <89908520+IamQinnn@users.noreply.github.com>
2025-07-29 11:36:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-const navbarLinks = document.querySelectorAll(".nav-menu .nav-link");
-const menuOpenButton = document.querySelector("#menu-open-button");
-const menuCloseButton = document.querySelector("#menu-close-button");
+const menuOpenBtn = document.getElementById("menu-open-button");
+const menuCloseBtn = document.getElementById("menu-close-button");
+const mobileMenu = document.getElementById("mobileMenu");
 
-menuOpenButton.addEventListener("click", () => {
-  // Toggle mobile menu visibility
-  document.body.classList.toggle("show-mobile-menu");
+menuOpenBtn.addEventListener("click", () => {
+  mobileMenu.style.display = "flex";
 });
 
-// Close menu when the close button is clicked
-menuCloseButton.addEventListener("click", () => menuOpenButton.click());
-
-// Close menu when nav link is clicked
-navbarLinks.forEach((link) => {
-  link.addEventListener("click", () => menuOpenButton.click());
+menuCloseBtn.addEventListener("click", () => {
+  mobileMenu.style.display = "none";
 });
+
 
 /* Initializing Swiper */
 let swiper = new Swiper(".slider-wrapper", {
